88c4b80c39e5f62f5cd6b86c8f14d2c8538f8167
@phpsturm <vla78dan@gmail.com>
2025-01-13 21:45:00 +0300

    Udemy_WEB2.0_phpitter_part1_21-00_script_SignUP

--- views/footer.php
+++ views/footer.php
@@ -6,29 +6,61 @@
 </footer>
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
+
+
+
 <!-- -----------------------Modal-------------------------------- -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Login</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                ...
+                <form>
+
+                    <!-- Скрытый input -->
+                    <input type="hidden" name="loginActive" value="1">
+                    <!-- END Скрытый input -->
+
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="email" aria-describedby="emailHelp"
+                               placeholder="Email address">
+                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password" placeholder="Password">
+                    </div>
+
+                </form>
             </div>
             <div class="modal-footer">
+                <a role="button" id="toggleLogin">Sign up</a>
+
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-primary">Login</button>
             </div>
         </div>
     </div>
 </div>
 
 <!-- -----------------------END Modal-------------------------------- -->
+
+
+
+<script>
+
+    $("#toggleLogin").click(function () {
+        alert("hi!");
+
+    });
+
+
+</script>
+
 
 </body>
 </html>
